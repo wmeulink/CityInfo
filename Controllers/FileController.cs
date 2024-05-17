f4a5ed93c93c3c6339421e376edd6aa8497f9b56
Whitney Meulink <wmeulink@gmail.com>
2024-05-17 13:29:00 -0700

    Adding initial commit

---
+++ Controllers/FileController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+        }
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            // look up actual file, depending on fileId
+            var pathToFile = "getting-started-with-rest-slides.pdf";
+            // check whether the file exists
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> CreateFile(IFormFile file)
+        {
+            if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+            {
+                return BadRequest("No file or an invalid one has been inputted");
+            }
+
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("Your file has been uploaded successfully.");
+        }
+    }
+}
---
+++ Models/CityDto.cs
@@ -0,0 +1,20 @@
+﻿namespace CityInfo.Models
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
+    }
+}
+ 
